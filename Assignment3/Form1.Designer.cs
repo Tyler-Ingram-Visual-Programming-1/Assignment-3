2faecb048407c0d9c13069b02494f799ba2ebd25
Tyler Ingram <tylerphx@gmail.com>
2021-03-19 14:37:04 -0500

    - Initial Commit

---
+++ Assignment3/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Assignment3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.addWorkShopButton = new System.Windows.Forms.Button();
+            this.calculateTotalButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.workShopListBox = new System.Windows.Forms.ListBox();
+            this.locationListBox = new System.Windows.Forms.ListBox();
+            this.costListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(109, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total Cost:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Location = new System.Drawing.Point(196, 163);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(100, 22);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // addWorkShopButton
+            // 
+            this.addWorkShopButton.Location = new System.Drawing.Point(52, 236);
+            this.addWorkShopButton.Name = "addWorkShopButton";
+            this.addWorkShopButton.Size = new System.Drawing.Size(87, 34);
+            this.addWorkShopButton.TabIndex = 4;
+            this.addWorkShopButton.Text = "Add Workshop";
+            this.addWorkShopButton.UseVisualStyleBackColor = true;
+            this.addWorkShopButton.Click += new System.EventHandler(this.addWorkShopButton_Click);
+            // 
+            // calculateTotalButton
+            // 
+            this.calculateTotalButton.Location = new System.Drawing.Point(145, 236);
+            this.calculateTotalButton.Name = "calculateTotalButton";
+            this.calculateTotalButton.Size = new System.Drawing.Size(88, 34);
+            this.calculateTotalButton.TabIndex = 5;
+            this.calculateTotalButton.Text = "Calculate Total";
+            this.calculateTotalButton.UseVisualStyleBackColor = true;
+            this.calculateTotalButton.Click += new System.EventHandler(this.calculateTotalButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(239, 236);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(94, 34);
+            this.resetButton.TabIndex = 6;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(339, 236);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(98, 34);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // workShopListBox
+            // 
+            this.workShopListBox.FormattingEnabled = true;
+            this.workShopListBox.Items.AddRange(new object[] {
+            "Handling Stress",
+            "Time Management",
+            "Supervision Skills",
+            "Negotiation",
+            "How to Interview"});
+            this.workShopListBox.Location = new System.Drawing.Point(25, 40);
+            this.workShopListBox.Name = "workShopListBox";
+            this.workShopListBox.Size = new System.Drawing.Size(104, 108);
+            this.workShopListBox.TabIndex = 8;
+            // 
+            // locationListBox
+            // 
+            this.locationListBox.FormattingEnabled = true;
+            this.locationListBox.Items.AddRange(new object[] {
+            "Austin",
+            "Chicago",
+            "Dallas",
+            "Orlando",
+            "Phoenix",
+            "Raleigh"});
+            this.locationListBox.Location = new System.Drawing.Point(145, 40);
+            this.locationListBox.Name = "locationListBox";
+            this.locationListBox.Size = new System.Drawing.Size(88, 108);
+            this.locationListBox.TabIndex = 9;
+            // 
+            // costListBox
+            // 
+            this.costListBox.FormattingEnabled = true;
+            this.costListBox.Location = new System.Drawing.Point(251, 40);
+            this.costListBox.Name = "costListBox";
+            this.costListBox.Size = new System.Drawing.Size(226, 108);
+            this.costListBox.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Pick a Workshop";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Pick a Location";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(248, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Workshop and Location";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(439, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Cost";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 282);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.costListBox);
+            this.Controls.Add(this.locationListBox);
+            this.Controls.Add(this.workShopListBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.calculateTotalButton);
+            this.Controls.Add(this.addWorkShopButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button addWorkShopButton;
+        private System.Windows.Forms.Button calculateTotalButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ListBox workShopListBox;
+        private System.Windows.Forms.ListBox locationListBox;
+        private System.Windows.Forms.ListBox costListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Assignment3/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+    {
+            InitializeComponent();
+    }
+        //Written by Tyler Ingram in 2020
+        //Adds the items to listboxes when the program is run
+        //Not using constants for efficiency  
+        double wshopcost = 0, lodge = 0, subtotal = 0, grandtotal =0;
+        string hostCity, workshop; 
+    private void addWorkShopButton_Click(object sender, EventArgs e)
+    {
+            Process();
+    }
+    private void Process()
+        {
+            //Calculates the total for the handling stress workshop and for any locations they choose
+            //and this is added to the costs textbox. Does this for every workshop.
+                    if (workShopListBox.SelectedIndex == 0 && locationListBox.SelectedIndex == 0)
+                    {
+                        wshopcost = 595;
+                        lodge = 3 * 95;
+                    }
+                    else if (workShopListBox.SelectedIndex == 0 && locationListBox.SelectedIndex == 1)
+                    {
+                        wshopcost = 595;
+                        lodge = 125 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 0 && locationListBox.SelectedIndex == 2)
+                    {
+                        wshopcost = 595;
+                        lodge = 110 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 0 && locationListBox.SelectedIndex == 3)
+                    {
+                        wshopcost = 595;
+                        lodge = 100 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 0 && locationListBox.SelectedIndex == 4)
+                    {
+                        wshopcost = 595;
+                        lodge = 92 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 0 && locationListBox.SelectedIndex == 5)
+                    {
+                        wshopcost = 595;
+                        lodge = 90 * 3;
+                    }
+                    //Calculates the total for the time management workshop and for any locations they choose, 
+                    //and this is added to the costs textbox 
+                    else if (workShopListBox.SelectedIndex == 1 && locationListBox.SelectedIndex == 0)
+                    {
+                        wshopcost = 695;
+                        lodge = 95 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 1 && locationListBox.SelectedIndex == 1)
+                    {
+                        wshopcost = 695;
+                        lodge = 125 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 1 && locationListBox.SelectedIndex == 2)
+                    {
+                        wshopcost = 695;
+                        lodge = 110 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 1 && locationListBox.SelectedIndex == 3)
+                    {
+                        wshopcost = 695;
+                        lodge = 125 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 1 && locationListBox.SelectedIndex == 4)
+                    {
+                        wshopcost = 695;
+                        lodge = 92 * 3;
+                    }
+                    else if (workShopListBox.SelectedIndex == 1 && locationListBox.SelectedIndex == 5)
+                    {
+                        wshopcost = 695;
+                        lodge = 90 * 3;
+                       
+                    }
+                    //Calculates the total for the supervision skills workshop and for any locations they choose, 
+                    //and this is added to the costs textbox 
+                    else if (workShopListBox.SelectedIndex == 2 && locationListBox.SelectedIndex == 0)
+                    {
+                        wshopcost = 995;
+                        lodge = 95 * 3;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 2 && locationListBox.SelectedIndex == 1)
+                    {
+                        wshopcost = 995;
+                        lodge = 125 * 3;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 2 && locationListBox.SelectedIndex == 2)
+                    {
+                        wshopcost = 995;
+                        lodge = 110 * 3;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 2 && locationListBox.SelectedIndex == 3)
+                    {
+                        wshopcost = 995;
+                        lodge = 100 * 3;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 2 && locationListBox.SelectedIndex == 4)
+                    {
+                        wshopcost = 995;
+                        lodge = 92 * 3;
+                       
+                    }
+                    else if (workShopListBox.SelectedIndex == 2 && locationListBox.SelectedIndex == 5)
+                    {
+                        wshopcost = 995;
+                        lodge = 90 * 3;
+                        
+                    }
+                    //Calculates the total for the Negotation workshop and for any locations they choose, 
+                    //and this is added to the  costs textbox 
+                    else if (workShopListBox.SelectedIndex == 3 && locationListBox.SelectedIndex == 0)
+                    {
+                        wshopcost = 1295;
+                        lodge = 95 * 5;
+                    }
+                    else if (workShopListBox.SelectedIndex == 3 && locationListBox.SelectedIndex == 1)
+                    {
+                        wshopcost = 1295;
+                        lodge = 125 * 5;
+                       
+                    }
+                    else if (workShopListBox.SelectedIndex == 3 && locationListBox.SelectedIndex == 2)
+                    {
+                        wshopcost = 1295;
+                        lodge = 110 * 5;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 3 && locationListBox.SelectedIndex == 3)
+                    {
+                        wshopcost = 1295;
+                        lodge = 100 * 5;
+                    }
+                    else if (workShopListBox.SelectedIndex == 3 && locationListBox.SelectedIndex == 4)
+                    {
+                        wshopcost = 1295;
+                        lodge = 92 * 5;
+                       
+                    }
+                    else if (workShopListBox.SelectedIndex == 3 && locationListBox.SelectedIndex == 5)
+                    {
+                        wshopcost = 1295;
+                        lodge = 90 * 5;
+                       
+                    }
+                    //Calculates the total for the How to Interview workshop and for any locations they choose, 
+                    //and this is added to the list of costs textbox 
+                    else if (workShopListBox.SelectedIndex == 4 && locationListBox.SelectedIndex == 0)
+                    {
+                        wshopcost = 395;
+                        lodge = 95 * 1;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 4 && locationListBox.SelectedIndex == 1)
+                    {
+                        wshopcost = 395;
+                        lodge = 125 * 1;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 4 && locationListBox.SelectedIndex == 2)
+                    {
+                        wshopcost = 395;
+                        lodge = 110 * 1;
+                       
+                    }
+                    else if (workShopListBox.SelectedIndex == 4 && locationListBox.SelectedIndex == 3)
+                    {
+                        wshopcost = 395;
+                        lodge = 100 * 1;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 4 && locationListBox.SelectedIndex == 4)
+                    {
+                        wshopcost = 395;
+                        lodge = 92 * 1;
+                        
+                    }
+                    else if (workShopListBox.SelectedIndex == 4 && locationListBox.SelectedIndex == 5)
+                    {
+                        wshopcost = 395;
+                        lodge = 90 * 1;
+                       
+                    }
+            hostCity = locationListBox.SelectedItem.ToString();
+            workshop = workShopListBox.SelectedItem.ToString();
+            subtotal = wshopcost + lodge;
+            grandtotal = grandtotal + subtotal;
+            costListBox.Items.Add(workshop + "\t" + hostCity + "\t" + subtotal.ToString("C"));     
+    }
+    private void calculateTotalButton_Click(object sender, EventArgs e)
+        {
+            OutputGrandTotal();
+        }
+    private void OutputGrandTotal()
+        {
+            outputLabel.Text = grandtotal.ToString("c");
+        }
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            ResetApplication();
+            ClearFields();
+        }
+        private void ResetApplication()
+        {
+            locationListBox.ClearSelected();
+            workShopListBox.ClearSelected();
+        }
+        private void ClearFields()
+        {
+            outputLabel.Text = "";
+            wshopcost = 0; lodge = 0; subtotal = 0; grandtotal = 0; 
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            ExitApplication();
+        }
+        private void ExitApplication()
+        {
+            Application.Exit();
+        }
+    }
+}
